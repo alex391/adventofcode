5c9bb636ae93791cffb91f3f83d896dd1a8861d2
Alex Leute <acl2809@rit.edu>
2020-12-04 01:46:47 -0500

    broken but nearly there

---
+++ day4.py
@@ -0,0 +1,25 @@
+import re
+MANDATORY_FIELDS = {
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+}
+print(MANDATORY_FIELDS)
+with open("day4.txt") as file:
+    fields = set()
+    valid = 0
+    for line in file:
+        #if the line is just \n, delete the set
+        if line == "\n":
+            if MANDATORY_FIELDS.issubset(fields):
+                valid += 1
+            fields = set()
+        else:
+           for i in re.split("[: ]",line.strip()):
+               fields.add(i)
+        print (fields)
+    print(valid)
